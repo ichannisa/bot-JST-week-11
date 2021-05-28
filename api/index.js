53c9e2fda4d0124a9e7d8e53c44f5007204b177f
ichannisa <84970095+ichannisa@users.noreply.github.com>
2021-05-28 19:12:59 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '1554577227:AAFj-Np-BITQ-HX_PDSvoosgXl1o6vc9fp4'
 const bot = new TelegramBot(token, {polling: true});
 
 
